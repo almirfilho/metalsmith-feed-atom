dd5cac280b0910bd0f8079e0e3edb8b297606c00
Almir Filho <almirflorenciofilho@gmail.com>
2016-04-29 15:57:54 -0300

    Adds initialization checks

--- src/feed.js
+++ src/feed.js
@@ -1,5 +1,17 @@
-module.exports = () => {
+module.exports = options => {
+  options = options || {};
+
+  if(!options.collection) throw new Error('`collection` option is required.');
+
   return (files, metalsmith, done) => {
+    var metadata = metalsmith.metadata();
+
+    if(!metadata.collections)
+      throw new Error('there are no collections configured - see metalsmith-collections plugin.');
+
+    if(!(options.collection in metadata.collections))
+      throw new Error(`the collection '${options.collection}' does not exist.`);
+
     done();
   };
 };
--- test/feed.test.js
+++ test/feed.test.js
@@ -1,13 +1,43 @@
 const expect = require('chai').expect;
+const metalsmith = require('metalsmith');
+const collections = require('metalsmith-collections');
 const feed = require('../src/feed');
 
 
-describe('Atom feed generator plugin', () => {
+describe('Initialization', () => {
+  var metal;
+
+  before(() => {
+    metal = metalsmith('test/fixtures');
+  });
+
   it('should be a function', () => {
     expect(feed).to.be.a.function;
   });
 
   it('should return a function', () => {
-    expect(feed()).to.be.a.function;
-  })
+    expect(feed({ collection: 'something' })).to.be.a.function;
+  });
+
+  it('should require collection name', () => {
+    expect(feed).to.throw(Error);
+  });
+
+  it('should require collections to be configured', () => {
+    metal._metadata = {};
+    var call = feed({ collection: 'name' }).bind(null, [], metal, () => {});
+    expect(call).to.throw(Error);
+  });
+
+  it('should throw error if collection doesnt exist', () => {
+    metal._metadata.collections = {};
+    var call = feed({ collection: 'posts' }).bind(null, [], metal, () => {});
+    expect(call).to.throw(Error);
+  });
+
+  it('should not throw error if collection exist', () => {
+    metal._metadata.collections = { articles: {} };
+    var call = feed({ collection: 'articles' }).bind(null, [], metal, () => {});
+    expect(call).not.to.throw(Error);
+  });
 });
